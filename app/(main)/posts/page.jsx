efbeb719e47b4cddbb04a64e7ad3c9a8f2dc3cd1
Sıla <silakara1453@gmail.com>
2024-10-18 22:58:05 +0300

    first commit

---
+++ app/(main)/posts/page.jsx
@@ -0,0 +1,37 @@
+import { createClient } from "@/utils/supabase/server";
+import LikeButton from "@/components/likes-btn/LikeButton";
+import BookmarkButton from "@/components/BookmarkButton/page";
+import "./allPost.css";
+
+export default async function PostsPage() {
+  const supabase = createClient();
+  const { data: posts, error } = await supabase.from("posts").select();
+
+  if (error) {
+    console.error(error);
+    return <div>Bir hata oluştu.</div>;
+  }
+
+  return (
+    <div className="allPost">
+      <h1>Tüm Gönderiler</h1>
+      <ul>
+        {posts.map(post => (
+          <div className="Posts" key={post.id}>
+            <li>
+              <div className="posts-title">
+                <a href={`/posts/${post.id}`}>{post.title}</a> <br />
+              </div>
+              <div className="post-buttons">
+                <LikeButton />
+                <BookmarkButton />
+              </div>
+            </li>
+            <hr />
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+
+}
